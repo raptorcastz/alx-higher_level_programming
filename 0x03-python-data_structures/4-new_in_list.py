dc7daf6c165d5c94151272d8fa44f1a0834bb6ff
Tdoms <chachaamaziah@gmail.com>
2023-08-13 21:50:01 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,13 +3,14 @@
 
 
 def new_in_list(my_list, idx, element):
-    # make a copy of the original list.
-    # We'll use list slicing [:] as an easy way to make a copy.
-    # Check if the index is in range. If it's not, return the original list.
-    # Otherwise, replace the specified element and return the new list.
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
+    # Try make a copy of the original list.
+    # then use list slicing [:] as an easy way to make a copy.
+    copy_list = my_list[:]
 
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    # Check if the index is in range. If it's not, return the original list.
+    if idx < 0 or idx >= len(copy_list):
+        return copy_list
+
+    # Otherwise, replace the specified element and return the new list.
+    copy_list[idx] = element
+    return copy_list
