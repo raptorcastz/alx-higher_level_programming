d18dba9fac22f377588e143ba60e13bd8a328308
Tdoms <chachaamaziah@gmail.com>
2023-08-16 21:47:32 +0300

    update

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,27 +1,51 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+def add(a, b):
+   My addition function
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    Args:
+        a: first integer
+        b: second integer
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+    Returns:
+        The return value. a + b
 
-result = None
+    return (a + b)
 
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
 
-print(f"{a} {operator} {b} = {result}")
+def sub(a, b):
+    My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+
+    return (a - b)
+
+
+def mul(a, b):
+    My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+
+    return (a * b)
+
+
+def div(a, b):
+    My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+
+    return int(a / b)
